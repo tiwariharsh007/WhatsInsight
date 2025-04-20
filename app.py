525a43ffd7dfc01a598aa928eac1d0d8bd3404d3
tiwariharsh007 <tiwari007harsh@gmail.com>
2025-04-20 19:38:37 +0530

    Basic Analysis Added.

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import preprocessor
+import helper
+
+st.sidebar.title("WhatsApp Chat Analyzer")
+st.sidebar.subheader("Upload your WhatsApp chat file")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
+
+    st.dataframe(df)
+
+    # fetch unique users
+    user_list = df['user'].unique().tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0,"Overall")
+
+    selected_user = st.sidebar.selectbox("Show analysis wrt",user_list)
+
+    if st.sidebar.button("Show Analysis"):
+
+        # Stats Area
+        num_messages, words, num_media_messages, num_links  = helper.fetch_stats(selected_user,df)
+        st.title("Top Statistics")
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.header("Total Messages")
+            st.title(num_messages)
+        with col2:
+            st.header("Total Words")
+            st.title(words)
+        with col3:
+            st.header("Media Shared")
+            st.title(num_media_messages)
+        with col4:
+            st.header("Links Shared")
+            st.title(num_links)
